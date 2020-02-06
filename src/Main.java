7c8222622196f47444aa52949e1c8bcf1d732333
Yevhenii Zakharenkov <evgen.fw@gmail.com>
2020-02-06 22:53:44 +0200

    refactring

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        String palindrom = "Madam, I'm Adam!";
-        char[] charfromarr = palindrom.replaceAll("[^A-Za-z1-9]+", "").toLowerCase().toCharArray();
-        Palindrome p = new Palindrome(charfromarr);
+        String palindrome = "Madam, I'm Adam!";
+        Palindrome p = new Palindrome(palindrome);
 
         System.out.println(p.isPalindrom());
     }
--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,17 +1,19 @@
 public class Palindrome {
-    public char[] plndr;
+    public String plndr;
+    char[] stringtochar;
 
-    public Palindrome(char[] palindrom) {
-        plndr = palindrom;
+    public Palindrome(String palindrome) {
+        plndr = palindrome;
     }
 
     public boolean isPalindrom() {
         int fromBegin = 0;
-        int fromEnd = plndr.length - 1;
+        stringtochar = plndr.replaceAll("[^A-Za-z1-9]+", "").toLowerCase().toCharArray();
+        int fromEnd = stringtochar.length - 1;
         boolean ispalindrom = true;
 
         while (fromBegin < fromEnd) {
-            if(plndr[fromBegin] != plndr[fromEnd]){
+            if(stringtochar[fromBegin] != stringtochar[fromEnd]){
                 ispalindrom = false;
             }
             fromBegin++;
